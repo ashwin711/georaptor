777e59cf8649379475919b26c0d8dff46c04f11c
Ashwin Nair <ashwin@near.co>
2017-06-07 14:45:26 +0530

    setup file updated for new version

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ import setuptools
 setup(
   name = 'georaptor',
   py_modules = ['georaptor'],
-  version = '2.0.1',
+  version = '2.0.2',
   description = 'A Python Geohash Compression Tool',
   long_description = open('README.rst').read(),
   author = 'Ashwin Nair',
   author_email = 'ashwinnair.ua@gmail.com',
   license = "MIT",
   url = 'https://github.com/ashwin711/georaptor',
-  download_url = 'https://github.com/ashwin711/georaptor/tarball/2.0.1',
-  keywords = ['geohash', 'optimizer', 'compression'],
+  download_url = 'https://github.com/ashwin711/georaptor/tarball/2.0.2',
+  keywords = ['geohash', 'optimizer', 'compression', 'location'],
   classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Environment :: Console',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: Utilities'
   ],
   install_requires = [
@@ -29,5 +31,3 @@ setup(
 	georaptor=georaptor:main
   '''
 )
-
-
