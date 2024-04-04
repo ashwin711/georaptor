542020479782258fd63916cb7c8e08d83e417655
ashwin711 <ashwinnair.ua@gmail.com>
2024-04-04 11:16:00 +0530

    georaptor updated

--- georaptor.py
+++ georaptor.py
@@ -1,17 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import time
 import argparse
 from clint.textui import puts, indent, colored
 
-
 def read_file(filename):
     # Read file by lines into a set
     with open(filename) as f:
         geohashes = set(f.read().splitlines())
-
     return geohashes
-
 
 # Combination generator for a given geohash at the next level
 def get_combinations(string):
@@ -19,27 +16,23 @@ def get_combinations(string):
               'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     return [string + '{0}'.format(i) for i in base32]
 
-
 # Recursive optimization of the geohash set
 def compress(geohashes, minlevel, maxlevel):
     deletegh = set()
     final_geohashes = set()
     flag = True
     final_geohashes_size = 0
-    
+
     # Input size less than 32
     if len(geohashes) == 0:
         puts(colored.red('No geohashes found!\n'))
         return False
 
-
-    while flag == True:
-
+    while flag:
         final_geohashes.clear()
         deletegh.clear()
 
         for geohash in geohashes:
-
             geohash_length = len(geohash)
 
             # Compress only if geohash length is greater than the min level
@@ -49,7 +42,6 @@ def compress(geohashes, minlevel, maxlevel):
 
                 # Proceed only if not already processed
                 if part not in deletegh and geohash not in deletegh:
-
                     # Generate combinations
                     combinations = set(get_combinations(part))
 
@@ -62,7 +54,6 @@ def compress(geohashes, minlevel, maxlevel):
 
                     # Else add the geohash to the temp out and deleted set
                     else:
-
                         deletegh.add(geohash)
 
                         # Forced compression if geohash length is greater than max level after combination check failure
@@ -76,7 +67,6 @@ def compress(geohashes, minlevel, maxlevel):
                         flag = False
 
         final_geohashes_size = len(final_geohashes)
-
         geohashes.clear()
 
         # Temp output moved to the primary geohash set
@@ -84,9 +74,7 @@ def compress(geohashes, minlevel, maxlevel):
 
     return geohashes
 
-
 def main():
-
     start_time = time.time()
     output_file = 'output.csv'
     minlevel = 1
@@ -97,25 +85,17 @@ def main():
     parser.add_argument('input', help='input filename containing list of geohashes')
     parser.add_argument('--output', default='output.csv',
                         help='output filename containing a optimized list of geohashes (default: output.csv)')
-    parser.add_argument('--minlevel', default=1,
+    parser.add_argument('--minlevel', type=int, default=1,
                         help='minimum level of geohash (default: 1)')
-    parser.add_argument('--maxlevel', default=12,
+    parser.add_argument('--maxlevel', type=int, default=12,
                         help='maximum level of geohash  (default: 12)')
 
     args = parser.parse_args()
 
     filename = args.input
-
-    if 'output' in args:
-        output_file  = args.output
-
-    if 'minlevel' in args:
-        minlevel = args.minlevel
-
-    if 'maxlevel' in args:
-        maxlevel = args.maxlevel
-
-    fp = open(output_file, "a");
+    output_file = args.output
+    minlevel = args.minlevel
+    maxlevel = args.maxlevel
 
     puts(colored.green('\nReading the file'))
 
@@ -123,27 +103,26 @@ def main():
 
     puts(colored.green(str("{:,}".format(len(geohashes))) + ' geohashes read\n'))
 
-    time.sleep(1);
+    time.sleep(1)
 
     puts(colored.red('Starting compression\n'))
 
-    georaptor_out = compress(geohashes, int(minlevel), int(maxlevel))
+    georaptor_out = compress(geohashes, minlevel, maxlevel)
 
     puts(colored.red('Compressed to: ' + str("{:,}".format(len(georaptor_out)))))
 
     if georaptor_out != False:
-
         # Output to file
-        for geohash in georaptor_out:
-            fp.write(geohash+'\n')
+        with open(output_file, 'a') as fp:
+            for geohash in georaptor_out:
+                fp.write(geohash + '\n')
 
         puts(colored.red('\nCompression complete!'))
-
         time.sleep(1)
-
         puts(colored.white('\nOutput available at ' + output_file + '\n'))
 
-
     et = time.time() - start_time
+    puts(colored.green('Total execution time: ' + str(et) + ' seconds\n'))
 
-    puts(colored.green('Total execution time: '+str(et)+' seconds\n'))
+if __name__ == "__main__":
+    main()
